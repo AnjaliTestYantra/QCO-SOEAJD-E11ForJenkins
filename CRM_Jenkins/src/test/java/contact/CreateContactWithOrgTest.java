a46838036ef07dbc604dcf6efe1850ad91d26cd7
AnjaliTestYantra <anjali.paleth@gmail.com>
2024-10-18 20:28:03 +0530

    added contact woth org

---
+++ CRM_Jenkins/src/test/java/contact/CreateContactWithOrgTest.java
@@ -0,0 +1,11 @@
+package contact;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateContactWithOrgTest {
+	@Test
+	public void createContactWithOrg() {
+		Reporter.log("Created Contact with Organization Successfully", true);
+	}
+}
