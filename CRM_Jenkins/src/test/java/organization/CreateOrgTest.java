5afbf4ed5409eeade375201cd828caf664d16010
AnjaliTestYantra <anjali.paleth@gmail.com>
2024-10-18 19:29:05 +0530

    Commit for Jenkins

---
+++ CRM_Jenkins/src/test/java/organization/CreateOrgTest.java
@@ -0,0 +1,12 @@
+package organization;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateOrgTest {
+	@Test
+	public void createOrg() {
+		Reporter.log("Created Organization Successfully", true);
+	}
+
+}
