ba4b544967b3aee4496dc0ce8a07f4acad9e97e2
AnjaliTestYantra <anjali.paleth@gmail.com>
2024-10-19 19:43:47 +0530

    suite file for jenkins

--- CRM_Jenkins/src/test/java/contact/CreateContactWithOrgTest.java
+++ CRM_Jenkins/src/test/java/contact/CreateContactWithOrgTest.java
@@ -4,7 +4,7 @@ import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class CreateContactWithOrgTest {
-	@Test
+	@Test(groups = "RegressionTest")
 	public void createContactWithOrg() {
 		Reporter.log("Created Contact with Organization Successfully", true);
 	}
--- CRM_Jenkins/src/test/java/organization/CreateOrgTest.java
+++ CRM_Jenkins/src/test/java/organization/CreateOrgTest.java
@@ -3,10 +3,20 @@ package organization;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
-public class CreateOrgTest {
-	@Test
+public class  CreateOrgTest{
+	@Test(groups = "SmokeTest")
 	public void createOrg() {
 		Reporter.log("Created Organization Successfully", true);
+		String URL = System.getProperty("Url");
+		String BROWSER = System.getProperty("Browser");
+		String UNAME = System.getProperty("Username");
+		String PWD = System.getProperty("Password");
+		
+		System.out.println("Url is: "+URL);
+		System.out.println("Browser is: "+BROWSER);
+		System.out.println("Username is: "+UNAME);
+		System.out.println("Password is: "+PWD);
+		
 	}
 
 }
